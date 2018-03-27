8c419ca6e5ab214716d31151e0ac6622af78d7d4
1076486954 <1076486954@qq.com>
2018-03-27 15:58:07 +0800

    添加game相关js

---
+++ src/main/resources/static/js/game/pintu_1.js
@@ -0,0 +1,100 @@
+/** 初始化游戏 */
+LInit(60, "mygame", 390, 580, main);
+
+var imgBmpd;
+/** 游戏层 */
+var stageLayer, gameLayer, overLayer;
+/** 拼图块列表 */
+var blockList;
+/** 是否游戏结束 */
+var isGameOver;
+/** 用时 */
+var startTime, time, timeTxt;
+/** 步数 */
+var steps, stepsTxt;
+
+function main () {
+    /** 全屏设置 */
+    if (LGlobal.mobile) {
+        LGlobal.stageScale = LStageScaleMode.SHOW_ALL;
+    }
+    LGlobal.screen(LGlobal.FULL_SCREEN);
+
+    /** 添加加载提示 */
+    var loadingHint = new LTextField();
+    loadingHint.text = "资源加载中……";
+    loadingHint.size = 20;
+    loadingHint.x = (LGlobal.width - loadingHint.getWidth()) / 2;
+    loadingHint.y = (LGlobal.height - loadingHint.getHeight()) / 2;
+    addChild(loadingHint);
+
+    /** 加载图片 */
+    LLoadManage.load(
+        [
+            {path : "./js/Block.js"},
+            {name : "img", path : "./images/img.jpg"}
+        ],
+        null,
+        function (result) {
+            /** 移除加载提示 */
+            loadingHint.remove();
+
+            /** 保存位图数据，方便后续使用 */
+            imgBmpd = new LBitmapData(result["img"]);
+
+            gameInit();
+        }
+    );
+}
+
+function gameInit (e) {
+    /** 初始化舞台层 */
+    stageLayer = new LSprite();
+    stageLayer.graphics.drawRect(0, "", [0, 0, LGlobal.width, LGlobal.height], true, "#EFEFEF");
+    addChild(stageLayer);
+
+    /** 初始化游戏层 */
+    gameLayer = new LSprite();
+    stageLayer.addChild(gameLayer);
+
+    /** 初始化最上层 */
+    overLayer = new LSprite();
+    stageLayer.addChild(overLayer);
+
+    /** 添加开始界面 */
+    addBeginningUI();
+}
+
+function addBeginningUI () {
+    var beginningLayer = new LSprite();
+    beginningLayer.graphics.drawRect(0, "", [0, 0, LGlobal.width, LGlobal.height], true, "#EDEDED");
+    stageLayer.addChild(beginningLayer);
+
+    /** 游戏标题 */
+    var title = new LTextField();
+    title.text = "拼图游戏";
+    title.size = 50;
+    title.weight = "bold";
+    title.x = (LGlobal.width - title.getWidth()) / 2;
+    title.y = 160;
+    title.color = "#FFFFFF";
+    title.lineWidth = 5;
+    title.lineColor = "#000000";
+    title.stroke = true;
+    beginningLayer.addChild(title);
+
+    /** 开始游戏提示 */
+    var hint = new LTextField();
+    hint.text = "- 点击屏幕开始游戏 -";
+    hint.size = 25;
+    hint.x = (LGlobal.width - hint.getWidth()) / 2;
+    hint.y = 370;
+    beginningLayer.addChild(hint);
+
+    /** 开始游戏 */
+    beginningLayer.addEventListener(LMouseEvent.MOUSE_UP, function () {
+        beginningLayer.remove();
+
+        startGame();
+    });
+}
